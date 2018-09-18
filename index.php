0052fba4434d6ac09de0faae24088e181a8a057f
gerald prambudi <geraldprambudi94@gmail.com>
2018-09-18 08:38:59 +0700

    tambah post big & sidebar

--- index.php
+++ index.php
@@ -51,21 +51,28 @@
             </nav>
         </div>
 
-        <div class="row mb-2">
-            <div class="col-md-6">
-                <div class="card flex-md-row mb-4 box-shadow h-md-250">
-
-            <div class="card-body d-flex flex-column align-items-start">
-              <strong class="d-inline-block mb-2 text-primary">World</strong>
-              <h3 class="mb-0">
-                <a class="text-dark" href="#">Featured post</a>
-              </h3>
-              <div class="mb-1 text-muted">Nov 12</div>
-              <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-              <a href="#">Continue reading</a>
+        <div class="row">
+            <div class="card col-8">
+              <img class="card-img-top" src="assets/img/light.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Tutorial Fullstack Javascript</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text text-dark mb-0">Vanesha Prescilla</p>
+                <p class="card-text"><small class="text-muted">Aug 28</small></p>
+              </div>
             </div>
-            <img class="card-img-right flex-auto d-none d-md-block" data-src="https://unsplash.com/photos/-WhUhOB1O00.jpg">
-          </div>
+
+            <div class="card col-4">
+                <div class="card flex-md-row mb-4 box-shadow h-md-250">
+                    <div class="card-body d-flex flex-column align-items-start">
+                      <h5 class="px-5" style="text-align: justify;">
+                            <p class="card-text mb-auto text-dark">This is a wider</p>
+                      </h5>
+                      <p class="card-text mb-auto text-muted">This is a wider card with.</p>
+                      <p class="card-text text-dark mb-0"><small>Vanesha Prescilla</small></p>
+                      <p class="card-text"><small class="text-muted">Aug 28</small></p>
+                    </div>
+                    <img class="card-img-right flex-auto d-none d-md-block" src="assets/img/code.jpg" alt="Card image cap">
                 </div>
             </div>
         </div>
